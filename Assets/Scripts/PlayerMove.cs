20e6e30da48998401a1317ccf0c32f76cd896980
HereisLuvika <kth882500@gmail.com>
2025-01-20 13:52:46 +0900

    FollowingCamera

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,16 +2,28 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public Transform player;
+    public float moveSpeed = 5f;
+    public Transform followingCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        } 
+        else if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+        }
+    }
+    void LateUpdate()
+    {
+        followingCamera.position = new Vector3(transform.position.x, transform.position.y, -10);
     }
 }
