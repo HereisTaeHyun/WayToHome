075df36a794d739e0e6066e1dbd5cbe35ce864f6
HereisLuvika <kth882500@gmail.com>
2025-01-20 19:11:43 +0900

    jumpPlus

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,26 +4,32 @@ public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpSpeed = 5f;
-    public Transform followingCamera;
-    Rigidbody2D rigidbody2D;
+    public LayerMask ground;
+    public float groundDistance = 0.1f;
+    private bool isGround;
+    Rigidbody2D rigidBody;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.D))
+        float h = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(h, 0);
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
+
+        Vector2 rayStart = new Vector2(transform.position.x, transform.position.y - 1);
+        isGround = Physics2D.Raycast(rayStart, Vector2.down, groundDistance, ground);
+        if(Input.GetButtonDown("Jump") && isGround)
         {
-            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);
-        } 
-        else if(Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0, 0);
+            rigidBody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
     }
+
+    public Transform followingCamera;
     void LateUpdate()
     {
         followingCamera.position = new Vector3(transform.position.x, transform.position.y, -10);
