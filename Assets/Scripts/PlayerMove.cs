413bda6b5509e29bcffd4545facc5347527a8e86
HereisLuvika <kth882500@gmail.com>
2025-01-21 09:43:12 +0900

    주석 정리

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -17,18 +17,21 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 수평 입력 값을 받아 방향 벡터 변환 후 Translate로 이동 입력
         float h = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(h, 0);
         transform.Translate(movement * moveSpeed * Time.deltaTime);
-
+        
+        // 점프 가능 유무 방지를 발에서 시작한 ray가 Ground에 닿는지로 체크
         Vector2 rayStart = new Vector2(transform.position.x, transform.position.y - 1);
         isGround = Physics2D.Raycast(rayStart, Vector2.down, groundDistance, ground);
-        if(Input.GetButtonDown("Jump") && isGround)
+        if(Input.GetButtonDown("Jump") && isGround) // ray가 Ground에 닿으면 점프
         {
             rigidBody.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
         }
     }
 
+    // MainCamera가 Player의 중심을 추적하도록
     public Transform followingCamera;
     void LateUpdate()
     {
