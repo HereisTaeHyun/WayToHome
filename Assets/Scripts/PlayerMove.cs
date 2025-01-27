a1743075a9a83a53ed2e52e10caaa12cd510b123
HereisLuvika <kth882500@gmail.com>
2025-01-27 13:10:19 +0900

    점프카운트 추가

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,8 @@ public class PlayerMove : MonoBehaviour
     public float groundDistance = 0.3f;
     public bool isGround;
     Rigidbody2D rigidBody;
+    public int maxJump = 1;
+    public int jumpCount = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -24,10 +26,15 @@ public class PlayerMove : MonoBehaviour
         // 점프 가능 유무 방지를 발에서 시작한 ray가 Ground에 닿는지로 체크
         Vector2 rayStart = new Vector2(transform.position.x, transform.position.y - 0.9f);
         isGround = Physics2D.Raycast(rayStart, Vector2.down, groundDistance, ground);
-        Debug.DrawRay(rayStart, Vector2.down * groundDistance, Color.red);
-
-        if(Input.GetButtonDown("Jump") && isGround) // ray가 Ground에 닿으면 점프
+        if(isGround == true)
         {
+            jumpCount = 0;
+        }
+        // Debug.DrawRay(rayStart, Vector2.down * groundDistance, Color.red);
+
+        if(Input.GetButton("Jump") && jumpCount < maxJump) // ray가 Ground에 닿으면 점프
+        {
+            jumpCount += 1;
             rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpSpeed);
         }
     }
