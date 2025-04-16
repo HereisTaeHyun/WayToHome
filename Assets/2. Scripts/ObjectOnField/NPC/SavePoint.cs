fe08a2e288e81d4dc7d52137b0ebba5f6cfe040c
HereisLuvika <kth882500@gmail.com>
2025-04-16 12:48:54 +0900

    중간보스 제작 중

--- Assets/2. Scripts/ObjectOnField/NPC/SavePoint.cs
+++ Assets/2. Scripts/ObjectOnField/NPC/SavePoint.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class SavePoint : MonoBehaviour
 {
+    // stay 중 꾹 눌러서 프레임마다 저장되는 것 막기, 사운드 너무 이상함
+    private bool isSaved = false;
     [SerializeField] AudioClip saveSfx;
     void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player") && Input.GetButton("Submit"))
+        if(collision.gameObject.CompareTag("Player") && Input.GetButton("Submit") && isSaved == false)
         {
             // 스탯 저장 후 스폰 포인트를 해당 포인트로 설정
+            isSaved = true;
             GameManager.instance.SavePlayerStat();
             GameManager.instance.SetSpawnPos(gameObject.transform.position);
             UtilityManager.utility.PlaySFX(saveSfx);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            isSaved = false;
         }
     }
 }
