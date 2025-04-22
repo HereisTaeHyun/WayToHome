2d2e44150b7677225d6a476afab281517cba9bcd
HereisTaeHyun <kth882500@gmail.com>
2025-04-22 18:15:17 +0900

    보스룸 감지기 설치
    플레이어 진입시 보스 활성화 및 벽 설치
    
    보스 사망시 벽 제거

---
+++ Assets/2. Scripts/BossRoomSensor.cs
@@ -0,0 +1,37 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BossRoomSensor : MonoBehaviour
+{
+    [SerializeField] GameObject boss;
+    [SerializeField] GameObject leftWall;
+    [SerializeField] GameObject rightWall;
+
+    // 보스룸 오브젝트는 기본적으로 비활성화
+    void Start()
+    {
+        boss.SetActive(false);
+        leftWall.SetActive(false);
+        rightWall.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(boss == null)
+        {
+            leftWall.SetActive(false);
+            rightWall.SetActive(false);
+        }
+    }
+
+    // 플레이어가 센서 진입 시 보스룸 요소들 활성화
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            boss.SetActive(true);
+            leftWall.SetActive(true);
+            rightWall.SetActive(true);
+        }
+    }
+}
