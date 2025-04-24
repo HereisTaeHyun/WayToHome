653e4614661e667dfbfc83589222605c148feb16
HereisTaeHyun <kth882500@gmail.com>
2025-04-24 19:52:49 +0900

    Itemmanager todo 있음

--- Assets/2. Scripts/ItemManager.cs
+++ Assets/2. Scripts/ItemManager.cs
@@ -44,4 +44,8 @@ public class ItemManager : MonoBehaviour
         UtilityManager.utility.CreatePool(ref attakPlusPool, attackPlusItem, 3, 3);
         UtilityManager.utility.CreatePool(ref maxJumpPlusPool, maxJumpPlusItem, 1, 1);
     }
+
+    // 각 아이템 드롭하는 오브젝트들이 어떤 풀에서 가져와야할지 알게 하기 위해 작성해보기
+    // 아이템을 받고 스위치로 어떤 아이템인지에 따라 pool 지정해주기
+    // private void SelectPool
 }
