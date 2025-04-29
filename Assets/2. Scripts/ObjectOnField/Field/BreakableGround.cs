6fb8c3a80df7bac1f6e2761c0654329bd3a0f007
HereisLuvika <kth882500@gmail.com>
2025-04-29 12:48:30 +0900

    todo 주석 추가

--- Assets/2. Scripts/ObjectOnField/Field/BreakableGround.cs
+++ Assets/2. Scripts/ObjectOnField/Field/BreakableGround.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 public class BreakableGround : MonoBehaviour
 {
     [SerializeField] private float breakTime;
+
+    // todo 해당 코루틴을 player를 인자로 받아와 속도 등도 처리 가능하게 변경할 것
+    // playerMove를 받아와 그 속도롤 0으로 하는 로직이 필요할 듯
     public IEnumerator StartBreak()
     {
         PlayerCtrl.player.canMove = false;
