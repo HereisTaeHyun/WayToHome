144d97353c8bd72bbd6a160e6f0e2fcfd748552b
HereisLuvika <kth882500@gmail.com>
2025-05-04 16:57:06 +0900

    메테오 완

--- Assets/2. Scripts/Magic/Meteor.cs
+++ Assets/2. Scripts/Magic/Meteor.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class Meteor : MagicBase
 {
-    private Vector2 moveDir;
     private Vector2 newVelocity;
 
     protected override void Start()
@@ -11,5 +10,43 @@ public class Meteor : MagicBase
 
         moveSpeed = 5.0f;
         damage = -3.0f;
+    }
+
+    protected override void FixedUpdate()
+    {
+        MoveMagic();
+    }
+
+    private void MoveMagic()
+    {
+        if(GameManager.instance.readIsGameOver == false)
+        {
+            // 이동
+            newVelocity.Set(Vector2.down.x * moveSpeed, Vector2.down.y * moveSpeed);
+            rb2D.linearVelocity = newVelocity;
+        }
+    }
+
+    // Player, Wall, Ground 등에 닿으면 풀 리턴
+    protected override void OnTriggerStay2D(Collider2D other)
+    {    
+        if(GameManager.instance.readIsGameOver == false)
+        {
+            if(other.gameObject.CompareTag("Player"))
+            {
+
+                // 플레이어가 무적이 아니라면 공격
+                if(PlayerCtrl.player.readInvincible != true)
+                {
+                    PlayerCtrl.player.ChangeHP(damage);
+                    ReturnToOriginPool();
+                }
+            }
+            // 
+            else if(other.gameObject.CompareTag("Wall") || other.gameObject.CompareTag("Ground"))
+            {
+                ReturnToOriginPool();
+            }
+        }
     }
 }
