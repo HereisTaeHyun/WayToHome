5acd5546809ebc0bf3c10ef5f724c45517b27ad8
HereisTaeHyun <kth882500@gmail.com>
2025-05-19 13:15:26 +0900

    엔딩씬 사운드 작업

--- Assets/2. Scripts/ObjectOnField/Home.cs
+++ Assets/2. Scripts/ObjectOnField/Home.cs
@@ -1,12 +1,16 @@
+using System.Collections;
 using UnityEngine;
 
 public class Home : MonoBehaviour
 {
     private bool isEnd;
     private float distance;
+    private GameObject endPoint;
+    [SerializeField] private AudioClip getInSFX;
     void Start()
     {
         isEnd = false;
+        endPoint = transform.Find("EndPoint").gameObject;
     }
 
     // 플레이어와 집이 충분히 가까워지면 End 메서드 활성화
@@ -16,13 +20,16 @@ public class Home : MonoBehaviour
 
         if(distance <= 9.0f && isEnd == false)
         {
-            End();
+            StartCoroutine(End());
         }
     }
 
-    private void End()
+    private IEnumerator End()
     {
         isEnd = true;
         Debug.Log("End");
+
+        // 플레이어가 집에 들어간 후 페이드 아웃 후 엔딩씬으로
+        yield return new WaitForSeconds(2.0f);
     }
 }
