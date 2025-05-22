42ec4161232b3cb84d97a6ecd003bcabb70df27d
HereisTaeHyun <kth882500@gmail.com>
2025-05-22 12:37:48 +0900

    메뉴 페이드 기능 추가

--- Assets/2. Scripts/Menu/MenuCtrl.cs
+++ Assets/2. Scripts/Menu/MenuCtrl.cs
@@ -1,20 +1,62 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Image = UnityEngine.UI.Image;
 
 public class MenuCtrl : MonoBehaviour
 {
-    public void StartGame()
+    [SerializeField] private GameObject runningPlayer;
+    [SerializeField] private GameObject fade;
+    private Image fadeImage;
+    private float fadeOutTime = 1.5f;
+    private const float FADE_OUT_ALPHA = 1.0f;
+    private void Start()
     {
+        fadeImage = fade.GetComponent<Image>();
+    }
+
+    public void StartButton()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        StartCoroutine(ChangeAlpha(fadeImage, FADE_OUT_ALPHA, fadeOutTime));
+        yield return new WaitForSeconds(fadeOutTime);
         SceneManager.LoadScene(1);
     }
 
-    public void LoadGame()
+    public void LoadButton()
     {
 
     }
-    
-    public void ExitGame()
+
+    public void ExitButton()
     {
+        StartCoroutine(ExitGame());
+    }
+
+    private IEnumerator ExitGame()
+    {
+        StartCoroutine(ChangeAlpha(fadeImage, FADE_OUT_ALPHA, fadeOutTime));
+        yield return new WaitForSeconds(fadeOutTime);
         Application.Quit();
+    }
+    
+    // fade 아웃용 코루틴
+    public IEnumerator ChangeAlpha(Image changeTarget, float targetAlpah, float changeTime)
+    {
+        Color currentColor = changeTarget.color;
+        float time = 0.0f;
+
+        // 현재 알파가 목표 알파보다 작은 동안 점진적으로 알파 값 변경
+        while (currentColor.a <= targetAlpah)
+        {
+            time += Time.deltaTime / changeTime;
+            currentColor.a = Mathf.Lerp(0.0f, targetAlpah, time);
+            changeTarget.color = currentColor;
+            yield return null;
+        }
     }
 }
