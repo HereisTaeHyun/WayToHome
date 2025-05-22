d07c57926fa0d673179844c27c13aacb32f9e4b9
HereisTaeHyun <kth882500@gmail.com>
2025-05-22 18:30:57 +0900

    데이터 저장 패스 설정

--- Assets/2. Scripts/DataManager.cs
+++ Assets/2. Scripts/DataManager.cs
@@ -1,19 +1,38 @@
 using UnityEngine;
 
+    // 플레이어 데이터 구조 클래스
+[System.Serializable]
+public class PlayerData
+{
+    public float MaxHP;
+    public float currentHP;
+    public int money;
+    public int maxJump;
+    public float attackDamage;
+    public Vector2 currentSpawnPos;
+}
+
 public class DataManager : MonoBehaviour
 {
+    private static string savePath;
+
     // 싱글톤 선언
-    public static DataManager playerData = null;
+    public static DataManager dataManager = null;
     void Awake()
     {
-        if(playerData == null)
+        if(dataManager == null)
         {
-            playerData = this;
+            dataManager = this;
         }
-        else if(playerData != this)
+        else if(dataManager != this)
         {
             Destroy(this.gameObject);
         }
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        savePath = Application.persistentDataPath + "/save.json";
     }
 }
