3262fc6a951719f6dbdf43d4f58b2d3e720e914d
HereisTaeHyun <kth882500@gmail.com>
2025-06-11 15:09:06 +0900

    WaterSplash 1차완료 기능 필요

--- Assets/2. Scripts/Magic/ByPlayer/PlayerMagicBase.cs
+++ Assets/2. Scripts/Magic/ByPlayer/PlayerMagicBase.cs
@@ -6,6 +6,7 @@ public enum PlayerMagicType
     Kunai,
     Shuriken,
     SmallShockwave,
+    WaterSplash,
 }
 public class PlayerMagicBase : MonoBehaviour
 {
---
+++ Assets/2. Scripts/Magic/ByPlayer/WaterSplash.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.Pool;
+
+
+public class WaterSplash : PlayerMagicBase
+{
+    public PlayerMagicType playerMagicType;
+    private Vector2 moveDir;
+    private Collider2D coll2D;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        coll2D = GetComponent<Collider2D>();
+        coll2D.enabled = false;
+
+        moveSpeed = 5.0f;
+        damage = -1.0f;
+    }
+
+    protected override void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent<IDamageable>(out var target))
+        {
+            target.ChangeHP(damage);
+        }
+    }
+
+    public override void SetPool(ObjectPool<GameObject> pool)
+    {
+        if (coll2D == null)
+        {
+            coll2D = GetComponent<Collider2D>();
+        }
+        coll2D.enabled = false;
+
+        originPool = pool;
+        isPool = false;
+    }
+
+    // 애니메이션 이벤트로 제어
+    private void EnableAttackCollider()
+    {
+        coll2D.enabled = true;
+    }
+}
